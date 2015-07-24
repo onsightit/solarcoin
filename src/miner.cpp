ed3da9da6c06780efeee806d27efaa538adc6d49
onsightit <steve@onsightit.com>
2015-07-23 20:29:08 -0600

    testnet pow limits

--- src/miner.cpp
+++ src/miner.cpp
@@ -549,7 +549,7 @@ void StakeMiner(CWallet *pwallet)
                 return;
         }
 
-        while ((vNodes.size() < 5 && !fTestNet) || vNodes.size() < 2 || IsInitialBlockDownload() || nBestHeight < GetNumBlocksOfPeers())
+        while ((vNodes.size() < 5 && !fTestNet) || vNodes.size() < 1 || IsInitialBlockDownload() || nBestHeight < GetNumBlocksOfPeers())
         {
             nLastCoinStakeSearchInterval = 0;
             MilliSleep(60000);
