77802c4d13333118377ba2179ccec59cf3f11a1d
onsightit <steve@onsightit.com>
2015-08-02 14:51:37 -0600

    hack to get hashProofOfStake into a PoW index

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -471,21 +471,21 @@ bool CheckProofOfStakePoW(const CTransaction& tx, unsigned int nBits, uint256& h
         return error("CheckProofOfStakePoW() : called on non-coinbase %s", tx.GetHash().ToString().c_str());
 
     // Kernel (input 0) must match the stake hash target per coin age (nBits)
-    const CTxIn& txin = tx.vin[0];
+    const COutPoint txout = COutPoint(tx.GetHash(), 0);
 
-    // First try finding the previous transaction in database
-    CTxDB txdb("r");
-    CTransaction txPrev;
+    // First try finding the transaction in database
     CTxIndex txindex;
-    if (!txPrev.ReadFromDisk(txdb, txin.prevout, txindex))
-        return tx.DoS(1, error("CheckProofOfStakePoW() : INFO: read txPrev failed"));  // previous transaction not in main chain, may occur during initial download
+    if (!CTxDB("r").ReadTxIndex(tx.GetHash(), txindex))
+    {
+        return false;
+    }
 
     // Read block header
     CBlock block;
     if (!block.ReadFromDisk(txindex.pos.nFile, txindex.pos.nBlockPos, false))
         return fDebug? error("CheckProofOfStakePoW() : read block failed") : false; // unable to read block of previous transaction
 
-    if (!CheckStakeTimeKernelHash(nBits, block, txindex.pos.nTxPos - txindex.pos.nBlockPos, txPrev, txin.prevout, tx.nTime, hashProofOfStake, targetProofOfStake, pindexBest->pprev, fDebug))
+    if (!CheckStakeTimeKernelHash(nBits, block, txindex.pos.nTxPos - txindex.pos.nBlockPos, tx, txout, tx.nTime + nStakeMinAge, hashProofOfStake, targetProofOfStake, pindexBest->pprev, fDebug))
         return tx.DoS(1, error("CheckProofOfStakePoW() : INFO: check kernel failed on coinstake %s, hashProof=%s", tx.GetHash().ToString().c_str(), hashProofOfStake.ToString().c_str())); // may occur during initial download or if behind on block chain sync
 
     return true;
