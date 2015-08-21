c347ae8f421f6c1b9e76a9ca9f1d39b1ab1090e3
onsightit <steve@onsightit.com>
2015-08-21 16:32:58 -0600

    back to testnet version

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -248,8 +248,7 @@ static bool GetKernelStakeModifier(uint256 hashBlockFrom, uint64_t& nStakeModifi
     const CBlockIndex* pindexFrom = mapBlockIndex[hashBlockFrom];
     nStakeModifierHeight = pindexFrom->nHeight;
     nStakeModifierTime = pindexFrom->GetBlockTime();
-    // DEBUG TEST int64_t nStakeModifierSelectionInterval = (fTestNet ? nModifierInterval : GetStakeModifierSelectionInterval());
-    int64_t nStakeModifierSelectionInterval = GetStakeModifierSelectionInterval();
+    int64_t nStakeModifierSelectionInterval = (fTestNet ? nModifierInterval : GetStakeModifierSelectionInterval()); // DEBUG
     int64_t nStakeModifierTargetTime = nStakeModifierTime + nStakeModifierSelectionInterval;
     const CBlockIndex* pindex = pindexFrom;
 
