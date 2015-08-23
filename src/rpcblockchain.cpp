59c6074df540a25a298978985597a57f4f659f93
onsightit <steve@onsightit.com>
2015-08-22 22:36:03 -0600

    testing

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -108,7 +108,7 @@ double GetPoSKernelPS(CBlockIndex* pindexPrev)
 
     while (pindexPrev && nStakesHandled < nPoSInterval)
     {
-        if (pindexPrev->IsProofOfStake())
+        // DEBUG TEST if (pindexPrev->IsProofOfStake())
         {
             dStakeKernelsTriedAvg += GetDifficulty(pindexPrev) * 4294967296.0;
             nStakesTime += pindexPrevStake ? (pindexPrevStake->nTime - pindexPrev->nTime) : 0;
