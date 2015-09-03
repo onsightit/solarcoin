a09c8bb0d153f3aba734c92034c532a3ed1066ac
onsightit <steve@onsightit.com>
2015-09-03 16:46:10 -0600

    final initial coin supply numbers with 97.495B out of circulation

--- src/main.h
+++ src/main.h
@@ -27,10 +27,11 @@ class CRequestTracker;
 class CNode;
 
 static const int LAST_POW_BLOCK = 4000; // testnet
-static const int64_t INITIAL_COIN_SUPPLY = 9800390200; // testnet
+static const int64_t INITIAL_COIN_SUPPLY = 538646219; // testnet
 
-//static const int LAST_POW_BLOCK = 825000;               // If you change this, or mining rewards, adjust INITIAL_COIN_SUPPLY
-//static const int64_t INITIAL_COIN_SUPPLY = 98033732100; // Used in calculating interest rate
+//static const int LAST_POW_BLOCK = 800000;               // If you change this, or mining rewards, adjust INITIAL_COIN_SUPPLY
+//static const int64_t INITIAL_COIN_SUPPLY = 538646219; // Used in calculating interest rate (97.495085881B are out of circulation)
+
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/4;
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
