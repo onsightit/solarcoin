637667da8fa61322b15492eb5d4f2293cb2528e1
onsightit <steve@onsightit.com>
2015-09-12 05:25:24 -0600

    bootstrap bug fix

--- src/qt/downloader.cpp
+++ src/qt/downloader.cpp
@@ -112,7 +112,8 @@ void Downloader::on_quitButton_clicked() // Cancel button
         BitcoinGUI *p = qobject_cast<BitcoinGUI *>(parent());
         p->reloadBlockchainActionEnabled(true); // Set menu option back to true when dialog closes.
         processBlockchain = false;
-        fBootstrapTurbo = false;
+        if (!downloadFinished)
+            fBootstrapTurbo = false;
     }
     if (processUpdate)
     {
