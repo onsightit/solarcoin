d055bf17daa4fa7b5fbb65ae60eb84c04bf13dd1
Steve Woods <steve@onsightit.com>
2015-09-27 09:46:45 -0600

    remove expensive info from getinfo

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -89,18 +89,7 @@ Value getinfo(const Array& params, bool fHelp)
 
     if (pindexBest->nHeight > LAST_POW_BLOCK)
     {
-        double nNetworkWeight = GetAverageStakeWeight(pindexBest->pprev);
-    obj.push_back(Pair("networkweight", nNetworkWeight));
-        if (nNetworkWeight > 0)
-        {
-    obj.push_back(Pair("inflationrate", GetCurrentInflationRate(GetAverageStakeWeight(pindexBest->pprev))));
     obj.push_back(Pair("interestrate",  GetCurrentInterestRate(pindexBest->pprev)));
-        }
-        else
-        {
-    obj.push_back(Pair("inflationrate", "n/a"));
-    obj.push_back(Pair("interestrate",  "n/a"));
-        }
     }
 
     obj.push_back(Pair("coinsupply",    GetCurrentCoinSupply()));
