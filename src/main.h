afc58c01d74e4365f74e372ddcd21330252ce607
onsightit <steve@onsightit.com>
2016-04-28 09:53:45 -0600

    testnet fork

--- src/main.h
+++ src/main.h
@@ -30,7 +30,7 @@ class CNode;
 static const int LAST_POW_BLOCK = 8100; // testnet
 static const int64_t INITIAL_COIN_SUPPLY = 34145512; // testnet
 static const int TWO_PERCENT_INT_HEIGHT = LAST_POW_BLOCK + 10; // testnet
-static const int FORK_HEIGHT_1 = 8120;
+static const int FORK_HEIGHT_1 = 8125;
 
 // Mainnet
 //static const int LAST_POW_BLOCK = 835213;
