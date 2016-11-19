e05cda75739f93a64d9d80c39f5766c277c7b6c8
OnsightIT <onsightit@gmail.com>
2016-11-19 12:06:12 -0700

    Mac signed code/installer

--- release/macx_setup_builder.sh
+++ release/macx_setup_builder.sh
@@ -2,8 +2,8 @@
 
 # Note: The structure of this package depends on the -rpath,./lib to be set at compile/link time.
 
-year=`date +%Y`
 version="2.1.8"
+year=`date +%Y`
 arch=`uname -m`
 
 if [ "${arch}" = "x86_64" ]; then
@@ -18,24 +18,24 @@ if [ -f SolarCoin-Qt.app/Contents/MacOS/SolarCoin-Qt ] && [ -f solarcoin.conf ] 
     cp README SolarCoin-Qt.app/Contents/MacOS/
     cp doc/SolarCoinEula.* SolarCoin-Qt.app/Contents/MacOS/
     cp share/qt/Info.plist SolarCoin-Qt.app/Contents/
-    sed -ie "s/@VERSION@/$version/g" SolarCoin-Qt.app/Contents/Info.plist
-    sed -ie "s/@YEAR@/$year/g" SolarCoin-Qt.app/Contents/Info.plist
+    sed -e "s/@VERSION@/$version/g" -i "" SolarCoin-Qt.app/Contents/Info.plist
+    sed -e "s/@YEAR@/$year/g" -i "" SolarCoin-Qt.app/Contents/Info.plist
 
     # Remove the old archive
     if [ -f SolarCoin_${version}_${arch}.pkg ]; then
         rm -f SolarCoin_${version}_${arch}.pkg
     fi
 
-    # Deploy the app, create the plist, then build the package.
+    # Deploy app, sign, then build the package.
+    macdeployqt ./SolarCoin-Qt.app -always-overwrite
 
     # Sign the libs with the Developer Cert.
-    for f in `find ./SolarCoin-Qt.app/Contents -type f`; do
+    for f in `find ./SolarCoin-Qt.app -type f`; do
         codesign --force --verify --verbose --sign NDU9Q48556 $f
     done
     codesign --deep --force --verify --verbose --sign NDU9Q48556 ./SolarCoin-Qt.app
-    macdeployqt ./SolarCoin-Qt.app -always-overwrite
     pkgbuild --analyze --root ./SolarCoin-Qt.app share/qt/SolarCoin-Qt.plist
-    pkgbuild --root ./SolarCoin-Qt.app --component-plist share/qt/SolarCoin-Qt.plist --identifier org.solarcoin.SolarCoin-Qt --install-location /Applications/SolarCoin-Qt.app SolarCoin_${version}_${arch}.pkg
+    pkgbuild --root ./SolarCoin-Qt.app --component-plist share/qt/SolarCoin-Qt.plist --identifier com.OnsightIT.SolarCoin --install-location /Applications/SolarCoin-Qt.app SolarCoin_${version}_${arch}.pkg
     # Sign the app with the Installer Cert.
     productsign --sign R5V6NFP8XB SolarCoin_${version}_${arch}.pkg SolarCoin_${version}_${arch}.signed.pkg
     mv SolarCoin_${version}_${arch}.signed.pkg SolarCoin_${version}_${arch}.pkg
