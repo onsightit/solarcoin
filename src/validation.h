f49d3a041bb72bd1aa7fe0d2ba2267d1ab719cc7
OnsightIT <onsightit@gmail.co>
2017-11-30 07:22:35 -0700

    increase min blocks to keep if pruning

--- src/validation.h
+++ src/validation.h
@@ -206,7 +206,7 @@ extern bool fPruneMode;
 /** Number of MiB of block files that we're trying to stay below. */
 extern uint64_t nPruneTarget;
 /** Block files containing a block-height within MIN_BLOCKS_TO_KEEP of chainActive.Tip() will not be pruned. */
-static const unsigned int MIN_BLOCKS_TO_KEEP = 288;
+static const unsigned int MIN_BLOCKS_TO_KEEP = 512; // SolarCoin: Needs to be >= than block download limit of 500.
 
 static const signed int DEFAULT_CHECKBLOCKS = 6;
 static const unsigned int DEFAULT_CHECKLEVEL = 3;
